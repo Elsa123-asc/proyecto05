72b0d4e4cb6fce33655dd8afb59d4dff6dea6beb
elsa <elsa@gmail.com>
2024-11-15 14:05:20 +0100

    Nuevo fichero

---
+++ src/main/java/es/santander/ascender/proyecto05/Fichero.java
@@ -0,0 +1,13 @@
+public class Fichero {
+    public void leer (String nombre fichero)
+    valor = 1
+    FileInputStream fis  = new FileInputStream (nombreFichero);
+    BytesArrayOutputStream baos = new BytesArrayOutputStream ();
+    while (valor = fis read ()) ! = -1) {
+        baos.Write (valor);
+    }
+
+    retur.baos.toByteArray ();
+
+     
+}
