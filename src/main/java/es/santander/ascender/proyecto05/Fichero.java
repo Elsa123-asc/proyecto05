3f7b480f9be78bfab5679945f56e05c0d681d419
elsa <elsa@gmail.com>
2024-11-18 09:56:16 +0100

    Mejora de Fichero

--- src/main/java/es/santander/ascender/proyecto05/Fichero.java
+++ src/main/java/es/santander/ascender/proyecto05/Fichero.java
@@ -1,13 +1,54 @@
-public class Fichero {
-    public void leer (String nombre fichero)
-    valor = 1
-    FileInputStream fis  = new FileInputStream (nombreFichero);
-    BytesArrayOutputStream baos = new BytesArrayOutputStream ();
-    while (valor = fis read ()) ! = -1) {
-        baos.Write (valor);
+package es.santander.ascender.proyecto06;
+
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public abstract class Fichero implements Closeable {
+
+        // valor = 1;
+        // int a, b, c;
+        // a = b = c = 3;
+
+    public byte[] leer(String nombreFichero) throws IOException {
+        int valor;
+
+        FileInputStream fis = new FileInputStream(nombreFichero);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        while ((valor = fis.read()) != -1) {
+            baos.write(valor);
+        }
+
+        return baos.toByteArray();
     }
 
-    retur.baos.toByteArray ();
+    public byte[] leerEnBloques(String nombreFichero) throws IOException {
+        int cuantos;
+        byte[] valores = new byte[4096];
 
-     
+        FileInputStream fis = new FileInputStream(nombreFichero);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        while ((cuantos = fis.read(valores)) != -1) {
+            baos.write(valores, 0, cuantos);
+        }
+
+        return baos.toByteArray();
+    }
+
+    public void escribir(String nombreFichero, byte[] datos) throws IOException {
+        FileOutputStream fos = new FileOutputStream(nombreFichero);
+
+        fos.write(datos);
+    }
+
+    @Override
+    public void close() throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'close'");
+    }
+
+    public abstract void hacerElLunes();
+
 }
